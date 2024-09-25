3ea2e01f9e839d2a1dd56d4556d5dd744f31a247
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-09-25 14:30:00 +0500

    feat: bookAPI

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ef4at08!$0rg@oj6bi=$d_y&)q9prrtv@&ybxi8-h#tosdnjl*'
+SECRET_KEY = 'django-insecure-g0nu#!e##(dp$vh%cc*___8^1ai!_j$-kkm+ql#j^atezw_*^!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
